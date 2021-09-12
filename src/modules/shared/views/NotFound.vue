5be223dc7c8b153c667def40aebab7bf15e371f9
franciscomarin98 <francmarinc@gmail.com>
2021-09-12 18:55:08 -0500

    Conociendo la nueva forma para controlar las excepciones de ruta

---
+++ src/modules/shared/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>404 Not Page Found</h1>
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import Pokemon from "../modules/pokemon/views/Pokemon";
 import About from "../modules/pokemon/views/About";
 import List from "../modules/pokemon/views/List";
 import {createRouter, createWebHashHistory} from "vue-router";
+import NotFound from "../modules/shared/views/NotFound";
 
 
 const routes = [
@@ -13,6 +14,9 @@ const routes = [
     },
     {
         path: '/id', component: Pokemon
+    },
+    {
+        path: '/:pathMatch(.*)*', component: NotFound
     }
 ]
 
