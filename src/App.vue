496b327abbf2e2a88ee1264580180d402d536627
franciscomarin98 <francmarinc@gmail.com>
2021-09-12 19:38:51 -0500

    Agregando la funcionalidad para navegar entre paginas

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+    <Navbar/>
+    <img alt="Vue logo" src="./assets/logo.png">
     <router-view></router-view>
 </template>
 
 <script>
+import {defineAsyncComponent} from "vue";
+
 export default {
-  name: 'App',
-  components: {
-  }
+    name: 'App',
+    components: {
+        Navbar: defineAsyncComponent(() => import( /*webpackChunkName: "NavbarBundle"*/ './modules/shared/components/Navbar'))
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
