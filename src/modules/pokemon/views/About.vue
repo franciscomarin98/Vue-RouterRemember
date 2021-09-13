afcf4ab3bbc90bce179143e9fce3e4ba56c21bcc
franciscomarin98 <francmarinc@gmail.com>
2021-09-13 15:15:07 -0500

    Ananlizando los lyfecycle hooks de los componente en detalle

--- src/modules/pokemon/views/About.vue
+++ src/modules/pokemon/views/About.vue
@@ -1,10 +1,66 @@
 <template>
-<h1>About Page</h1>
+    <h1>About Page</h1>
+    <hr>
+    <h2>{{ name }}</h2>
+    <button @click="onChangeName">
+        Update name
+    </button>
 </template>
 
 <script>
 export default {
-    name: "About"
+    name: "About",
+    data() {
+        return {
+            name: 'Francisco'
+        }
+    },
+    methods: {
+        onChangeName() {
+            this.name = 'Joel'
+        }
+    },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        console.log('created')
+        //Aquí se recomienda hacer HTTP REQUEST
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate')
+    },
+    updated() {
+        console.log('updated')
+    },
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount')
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+    errorCaptured() {
+        console.log('errorCaptured')
+    },
+    renderTracked() {
+        console.log('renderTracked')
+    },
+    renderTriggered() {
+        console.log('renderTriggered')
+    }
+
 }
 </script>
 
