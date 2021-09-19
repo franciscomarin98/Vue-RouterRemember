558a85e95b21d7aa9d5b86fadc33d1aa816ebf7b
franciscomarin98 <francmarinc@gmail.com>
2021-09-18 22:42:57 -0500

    Creando un custom routerLink

---
+++ src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,38 @@
+<template>
+    <a v-if="isExternaLink" :href="link.to">{{ link.name }}</a>
+    <router-link
+        v-else
+        :to="link.to"
+        v-slot="{href, isExactActive}"
+    >
+        <a :href="href" :class="isExactActive ? 'active' : 'inactive'">{{ link.name }}</a>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "CustomLink",
+    props: {
+        link: {
+            type: Object,
+            required: true
+        },
+    },
+    computed: {
+        isExternaLink() {
+            return this.link.to.startsWith('http');
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+a.active {
+    color: #42b983;
+}
+
+a.inactive {
+    color: #2c3e50;
+}
+</style>
--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,14 +1,38 @@
 <template>
     <div>
-        <router-link :to="{name:'Home'}">Pokemon List</router-link>
-        <router-link :to="{name: 'Pokemon', params: {id: 500}}">Pokemon por id</router-link>
-        <router-link :to="{name: 'About'}">About</router-link>
+        <!--<router-link :to="{name:'Home'}">Pokemon List</router-link>-->
+        <!--<router-link :to="{name: 'Pokemon', params: {id: 500}}">Pokemon por id</router-link>-->
+        <!--<router-link :to="{name: 'About'}">About</router-link>-->
+        <CustomLink v-for="link in links" :key="link.name" :link="link"/>
     </div>
 </template>
 
 <script>
+import {defineAsyncComponent} from "vue";
+
 export default {
-    name: "Navbar"
+    name: "Navbar",
+    data() {
+        return {
+            links: [
+                {
+                    to: '/', name: 'Pokemons',
+                },
+                {
+                    to: '/pokemon-detail/16', name: 'Por ID',
+                },
+                {
+                    to: '/about', name: 'About',
+                },
+                {
+                    to: 'https://google.com', name: 'Google',
+                }
+            ]
+        }
+    },
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink'))
+    }
 }
 </script>
 
@@ -27,8 +51,7 @@ a {
     text-decoration: none;
 }
 
-a.router-link-active.router-link-exact-active {
-    color: #42b983;
-    text-decoration: underline;
-}
+/*a.router-link-active.router-link-exact-active {*/
+/*    color: #42b983;*/
+/*}*/
 </style>
