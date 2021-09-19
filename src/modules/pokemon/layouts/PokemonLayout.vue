b0d8773f3292926df883bac18482cb8ed4bea8dd
franciscomarin98 <francmarinc@gmail.com>
2021-09-18 23:03:55 -0500

    Implementando multiples children routes

---
+++ src/modules/pokemon/layouts/PokemonLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="pokemon-layout">
+        <p>Pokemon Layout</p>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PokemonLayout"
+}
+</script>
+
+<style scoped>
+.pokemon-layout {
+    background-color: burlywood;
+}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,27 +3,40 @@ import {createRouter, createWebHashHistory} from "vue-router";
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "ListBundle" */  '../modules/pokemon/views/List')
+        redirect: '/pokemon-layout'
     },
     {
-        path: '/about',
-        name: 'About',
-        component: () => import(/* webpackChunkName: "AboutBundle" */  '../modules/pokemon/views/About')
-    },
-    {
-        path: '/pokemon-detail/:id',
-        name: 'Pokemon',
-        component: () => import(/* webpackChunkName: "PokemonBundle" */ '../modules/pokemon/views/Pokemon'),
-        props: (route) => {
-            const pokemonId = Number(route.params.id)
-            return isNaN(pokemonId) ? {id: 1} : {id: pokemonId}
-        }
+        path: '/pokemon-layout',
+        name: 'PokemonLayout',
+        component: () => import(/* webpackChunkName: "PokemonLayout" */ '../modules/pokemon/layouts/PokemonLayout'),
+        children: [
+            {
+                path: 'home',
+                name: 'Home',
+                component: () => import(/* webpackChunkName: "ListBundle" */  '../modules/pokemon/views/List')
+            }, {
+                path: 'about',
+                name: 'About',
+                component: () => import(/* webpackChunkName: "AboutBundle" */  '../modules/pokemon/views/About')
+            },
+            {
+                path: 'pokemon-detail/:id',
+                name: 'Pokemon',
+                component: () => import(/* webpackChunkName: "PokemonBundle" */ '../modules/pokemon/views/Pokemon'),
+                props: (route) => {
+                    const pokemonId = Number(route.params.id)
+                    return isNaN(pokemonId) ? {id: 1} : {id: pokemonId}
+                }
+            },
+            {
+                path: '',
+                redirect: {name: 'Home'}
+            }
+        ]
     },
     {
         path: '/:pathMatch(.*)*',
         name: 'NotFound',
-        //redirect: '/'
         component: () => import(/*  webpackChunkName: "NotFoundBundle" */ '../modules/shared/views/NotFound')
     }
 ]
